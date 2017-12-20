8a86baadf41b1035e5c0150abc5b2aff0e369e11
FPPJono <34591633+FPPJono@users.noreply.github.com>
2017-12-21 08:34:55 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,7 @@ bot.on('message', message => {
         message.reply('this should work eventually :P');
     } else
     if (message.content.startsWith(PREFIX + "setGame")) {
-        if (message.author.id !== "270017125815418901"){
-            return message.reply("sorry, that command is only available for this bot's creator ")
-        }
-        let args = message.content.split(" ").slice(1);
-        let game = args.join(" ")
-        bot.user.setGame(game)
+
     }
     if (!message.content.startsWith(PREFIX)) return;
     
@@ -69,19 +64,38 @@ bot.on('message', message => {
                 "embed": {
                     "image": {
                         "url": "https://cdn.discordapp.com/attachments/391482844414738432/393114450606948352/passage534.png"
-                    },
-                    "author": {
-                        "name": "welcome to level 5",
-                        "icon_url": "https://github.com/FPPJono/puzzlebot/blob/master/puzzlebotbaconserver.jpg?raw=true"
-                        }
+                    }
                 }
             });
             console.log(message.author.username + " just completed level 4");
             break;
         case "01110000011000010111001101110011011000010110011101100101001101010011001100110100":
-            message.author.sendMessage("")
+            message.author.sendMessage("aaa aaaa abbb baa aaa ab")
             console.log(message.author.username + " just completed level 5");
-            break;        
+            break;       
+        case "011100110110100001101010011001000111001101100001":
+            message.author.sendMessage("this is the hardest level, good luck!", {
+                "embed": {
+                    "image": {
+                        "url": "https://cdn.discordapp.com/attachments/391482844414738432/393118029610745868/lvl7.jpg"
+                    }
+                }
+            });
+            console.log(message.author.username + " just completed level 6");
+            break;
+    }
+    switch (args[0].toLowerCase()) {
+        case "01110011011101000111001001100001011101110110001001100101011100100111001001111001"
+            message.author.sendMessage ("congrats, you solved the hardest puzzle in the game, heres an easy one\n **__I want to play a game__**")
+            console.log(message.author.username + "just completed level 7");
+            break;
+        case "setgame"
+            if (message.author.id !== "270017125815418901"){
+                return message.reply("sorry, that command is only available for this bot's creator ")
+            }
+            let args = message.content.split(" ").slice(1);
+            let game = args.join(" ")
+            bot.user.setGame(game)
     }
 });
 
