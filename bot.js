e85826cb2e584fd941954db1b5707990b60e657c
FPPJono <34591633+FPPJono@users.noreply.github.com>
2017-12-20 19:46:17 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ bot.on('message', message => {
     }
 });
 
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
 
     
     //member.addRole(member.guild.roles.find("name", "INSERTNAMEOFROLE"))
