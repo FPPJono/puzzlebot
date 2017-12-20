2088c22f7e5329c4eedbfc7695c540130632c353
FPPJono <34591633+FPPJono@users.noreply.github.com>
2017-12-20 15:59:21 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,38 +21,7 @@ bot.on('message', message => {
         let game = args.join(" ")
         bot.user.setGame(game)
     }
-    if (!message.content.startsWith(PREFIX)) return;
-    
-    var args = message.content.substring(PREFIX.length).split(" ");
-    
-    switch (args[0].toLowerCase()) {
-        case "scavengerhunt":
-            member.addRole(member.guild.roles.find("name", "scavengerhunt"))
-            break;
-        case "start":
-            msg.author.sendMessage("I see you've decided to join my hunt. this will be *very* easy, good luck!\n/p/BcyyoI7F8DN/*")
-            console.log(msg.author.username + " just began the hunt")
-            break;
-        case "0101010001001010010011100100011101001000":
-            msg.author.sendMessage("https://www.youtube.com/watch?v=Hb0Ghb1RQ7M")
-            console.log(msg.author.username + " just completed level 1")
-            break;
-        case "011101000110100001100101011100000110100101100011011010110110110001100101011001000110100101100100001110010011000100110001":
-            msg.author.sendMessage("had enough of decoding? Good, I have a riddle for you. \nWhat invention allows you to look right through a wall?")
-            console.log(msg.author.username + " just completed level 2")
-            break;
-        case "011101110110100101101110011001000110111101110111"
-            msg.author.sendMessage("lets go back to the beginning, may not be lvl 10 yet, but I think this'll be fine ;)\nAOLAYBLIHJVU")
-            console.log(msg.author.username + " just completed level 3")
-            break;
-        case "011101000110100001100101011101000111001001110101011001010110001001100001011000110110111101101110"
-            msg.author.sendMessage("picturewillgohere")
-            console.log(msg.author.username + " just completed level 4")
-            break;
-        case "01110000011000010111001101110011011000010110011101100101001101010011001100110100"
-            msg.author.sendMessage("")
-            console.log(msg.author.username + " just completed level 5")
-            break;
+
     }
 });
 
