e9a7bb719d9abfe33e845cb3742f2b146370f0a0
FPPJono <34591633+FPPJono@users.noreply.github.com>
2017-12-22 09:47:29 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,20 +32,32 @@ bot.on('message', message => {
         case "start":
             message.author.send("Ig see you've decided to join my hunt. this will be *very* easy, good luck!\n**/p/BcyyoI7F8DN/**")
             console.log(message.author.username + " just began the hunt");
+            userData[sender.id] = {
+                currentLevel: 1
+            }
             break;
         case "0101010001001010010011100100011101001000":
             message.author.send("good job! here's level 2\nhttps://www.youtube.com/watch?v=Hb0Ghb1RQ7M")
             console.log(message.author.username + " just completed level 1");
+            userData[sender.id] = {
+                currentLevel: 2
+            }
             break;
         case "011101000110100001100101011100000110100101100011011010110110110001100101011001000110100101100100001110010011000100110001":
             message.author.send("had enough of decoding? Good, I have a riddle for you. \nWhat invention allows you to look right through a wall?")
             console.log(message.author.username + " just completed level 2");
+            userData[sender.id] = {
+                currentLevel: 3
+            }
             break;
     }
     switch (args[0].toLowerCase()) {
         case "011101110110100101101110011001000110111101110111":
             message.author.send("lets go back to the beginning, may not be lvl 10 yet, but I think this'll be fine ;)\nAOLAYBLIHJVU")
             console.log(message.author.username + " just completed level 3");
+            userData[sender.id] = {
+                currentLevel: 4
+            }
             bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
         case "011101000110100001100101011100100110010101100001011011000110001001100001011000110110111101101110":
@@ -57,6 +69,9 @@ bot.on('message', message => {
                 }
             });
             console.log(message.author.username + " just completed level 4");
+            userData[sender.id] = {
+                currentLevel: 5
+            }
             bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
         case "010101000100100001000101010101000101001001010101010001010100001001000001010000110100111101001110":
@@ -68,11 +83,17 @@ bot.on('message', message => {
                 }
             });
             console.log(message.author.username + " just completed level 4");
+            userData[sender.id] = {
+                currentLevel: 5
+            }
             bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
         case "01110000011000010111001101110011011000010110011101100101001101010011001100110100":
             message.author.send("aaa aaaa abbb baa aaa ab")
             console.log(message.author.username + " just completed level 5");
+            userData[sender.id] = {
+                currentLevel: 6
+            }
             bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;       
         case "011100110110100001101010011001000111001101100001":
@@ -84,6 +105,9 @@ bot.on('message', message => {
                 }
             });
             console.log(message.author.username + " just completed level 6");
+            userData[sender.id] = {
+                currentLevel: 7
+            }
             bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
     }
@@ -91,6 +115,9 @@ bot.on('message', message => {
         case "01110011011101000111001001100001011101110110001001100101011100100111001001111001":
             message.author.send ("congrats, you solved the hardest puzzle in the game, heres an easy one\n**__I want to play a game__**")
             console.log(message.author.username + " just completed level 7");
+            userData[sender.id] = {
+                currentLevel: 8
+            }
             bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
         case "01100101011011100110010001111001011100110110100101110011":
@@ -98,6 +125,9 @@ bot.on('message', message => {
                 "files": ["https://cdn.discordapp.com/attachments/391482844414738432/393214243995910146/lvl9.zip"]
             });
             bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
+            userData[sender.id] = {
+                currentLevel: 9
+            }
             console.log(message.author.username + " just completed level 8")
             break;
         case "0110111001101001011000110110010101110100011000010110001101101111":
@@ -108,6 +138,9 @@ bot.on('message', message => {
                     }
                 }
             });
+            userData[sender.id] = {
+                currentLevel: 10
+            }
             console.log(message.author.username + " just completed level 9")
             break;
         case "0111001001100001011010010110111001100010011011110111011101110011":
@@ -138,10 +171,21 @@ bot.on('message', message => {
             }
             userData[sender.id].hintsLeft--;
             if (userData[sender.id].hintsLeft >= 0) {
-                message.author.send ('yeet')
+                if (userData[sender.id].currentLevel === 1) {
+                    message.author.send('**cough** *insta*__hexa__ **cough**')
+                }else
+                if (userData[sender.id].currentLevel === 2) {
+                    message.author.send('if I remember correctly, __socialblade__ may be helpful')
+                }else
+                if (userData[sender.id].currentLevel === 3) {
+                    message.author.send('you probably have at least one in your house')
+                }else
+                if (userData[sender.id].currentLevel === 4) {
+                    message.author.send ('This man was a former roman consul with a puzzle associated with him')
+                }
+                message.author.send('You have ' + userData[sender.id].hintsLeft + ' hints remaining.')
             }else
-                sender.send ('you have no hints left')
-            message.author.send('You have ' + userData[sender.id].hintsLeft + ' hints remaining.')
+                sender.send ("you have no hints left, so uh. looks like you're on your own for this. :P")
             fs.writeFile('userData.json', JSON.stringify(userData), (err) => {
                 if (err) console.error(err);
             });
