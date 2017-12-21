3a679923ded542f26d51cc9c26877c7a361131ec
FPPJono <34591633+FPPJono@users.noreply.github.com>
2017-12-21 14:02:07 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,20 +42,18 @@ bot.on('message', message => {
         case "011101110110100101101110011001000110111101110111":
             message.author.sendMessage("lets go back to the beginning, may not be lvl 10 yet, but I think this'll be fine ;)\nAOLAYBLIHJVU")
             console.log(message.author.username + " just completed level 3");
+            bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
         case "011101000110100001100101011101000111001001110101011001010110001001100001011000110110111101101110":
             message.author.sendMessage("hm, this should be interesting", {
                 "embed": {
                     "image": {
                         "url": "https://cdn.discordapp.com/attachments/391482844414738432/393114450606948352/passage534.png"
-                    },
-                    "author": {
-                        "name": "welcome to level 5",
-                        "icon_url": "https://github.com/FPPJono/puzzlebot/blob/master/puzzlebotbaconserver.jpg?raw=true"
-                        }
+                    }
                 }
             });
             console.log(message.author.username + " just completed level 4");
+            bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
         case "010101000100100001000101010101000101001001010101010001010100001001000001010000110100111101001110":
             message.author.sendMessage("hm, this should be interesting", {
@@ -66,10 +64,12 @@ bot.on('message', message => {
                 }
             });
             console.log(message.author.username + " just completed level 4");
+            bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
         case "01110000011000010111001101110011011000010110011101100101001101010011001100110100":
             message.author.sendMessage("aaa aaaa abbb baa aaa ab")
             console.log(message.author.username + " just completed level 5");
+            bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;       
         case "011100110110100001101010011001000111001101100001":
             message.author.sendMessage("this is the hardest level, good luck!", {
@@ -80,14 +80,25 @@ bot.on('message', message => {
                 }
             });
             console.log(message.author.username + " just completed level 6");
+            bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
     }
     switch (args[0].toLowerCase()) {
         case "01110011011101000111001001100001011101110110001001100101011100100111001001111001":
             message.author.sendMessage ("congrats, you solved the hardest puzzle in the game, heres an easy one\n**__I want to play a game__**")
             console.log(message.author.username + "just completed level 7");
+            bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
             break;
-        case "apple":
+        case "01100101011011100110010001111001011100110110100101110011":
+            message.author.sendMessage("see, I told you it was easy. now read this .txt", {
+                "embed": {
+                    "image": {
+                        "url": ""
+                    }
+                }
+            });
+            bot.user.setPresence({ game: { name: 'endysis', type: 0 } });
+            console.log(message.author.username + "just completed level 8")
             break;
     }
 });
